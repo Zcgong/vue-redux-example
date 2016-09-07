4ccd3ed6b8f0895d2a6ad6630d2425e669c851c4
Nadim Tuhin <nadimtuhin@gmail.com>
2016-09-07 16:56:12 +0600

    hello in vue-jsx

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,25 +1,29 @@
 import Rahul from './Rahul';
 import Rifat from './Rifat';
 import Saddam from './Saddam';
-import hoc from './hoc';
 
 // import store from '../store';
 
 const component = {
   components: { Rahul, Rifat, Saddam },
   props: ['msg', 'list', 'name'],
-  template: `<div>
-    {{ name }}
-    <div class="hello">
-      <h1>{{ msg }}!!</h1>
-      <p v-on:click="dontClickMe"> {{ test }} </p>
-    </div>
-    <div>
-      <rahul name='rahul'></rahul> <br>
-      <rifat> </rifat> <br>
-      <saddam> </saddam>
-    </div>
-</div>`,
+
+  render (h) { // <-- h must be in scope
+    return <div>
+      { this.name }
+      <div class="hello">
+        <h1>{ this.msg }!!</h1>
+        <p on-click={this.dontClickMe}> { this.test } </p>
+      </div>
+      <div>
+        <rahul name='rahul' />
+        <br />
+        <rifat />
+        <br />
+        <saddam />
+      </div>
+    </div>;
+  },
 
   data () {
     return {
@@ -41,6 +45,8 @@ const mapStateAsProps = (state) => {
   }
 };
 
+import hoc from './hoc';
+// import connect from './connect';
 export default hoc(mapStateAsProps)(component);
 // export default connect(mapStateAsProps)(component);
 // export default component;
